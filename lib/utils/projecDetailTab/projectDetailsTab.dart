5710a873bd1e37409959618931f1364aad51840a
RohanSengupta326 <rohansengupta326@gmail.com>
2024-01-03 12:11:55 +0530

    integrated markdown support in project description section

--- lib/utils/projecDetailTab/projectDetailsTab.dart
+++ lib/utils/projecDetailTab/projectDetailsTab.dart
@@ -1,10 +1,24 @@
 import 'package:bug_tracker/consts/const_values/ConstValues.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_markdown/flutter_markdown.dart';
+import 'package:url_launcher/url_launcher.dart';
 
 class ProjectDetailTab extends StatelessWidget {
   final projectName;
   final projectDetails;
   ProjectDetailTab({this.projectName, this.projectDetails});
+
+  Future<void> _launchUrl(String uri) async {
+    final Uri url = Uri.parse(uri);
+
+    if (!await launchUrl(
+      url,
+      mode: LaunchMode.externalApplication,
+      // to open in youtube
+    )) {
+      throw 'Could not launch $url';
+    }
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -30,11 +44,9 @@ class ProjectDetailTab extends StatelessWidget {
               margin: const EdgeInsets.only(
                   left: ConstValues.MARGIN, right: ConstValues.MARGIN),
               padding: const EdgeInsets.all(ConstValues.PADDING),
-              child: Text(
-                projectDetails,
-                style: const TextStyle(
-                  fontSize: ConstValues.FONT_SIZE,
-                ),
+              child: MarkdownBody(
+                data: projectDetails,
+                onTapLink: (text, href, title) => _launchUrl(href ?? ''),
               ),
             ),
           )
